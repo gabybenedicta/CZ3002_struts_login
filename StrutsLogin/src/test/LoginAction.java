3f4ae2aa1022a2c05c5e20dd4a0731ca82032a4a
Elaine <elainepakxinyi@gmail.com>
2020-11-18 02:31:58 +0800

    connect to database

--- StrutsLogin/src/test/LoginAction.java
+++ StrutsLogin/src/test/LoginAction.java
@@ -1,5 +1,12 @@
 package test;
+
 import com.opensymphony.xwork2.ActionSupport;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class LoginAction extends ActionSupport{
 	private static final long serialVersionUID = 1L;
@@ -21,12 +28,34 @@ public class LoginAction extends ActionSupport{
 		this.password= password;
 	}
 	
-	public String execute() {
-		if(uname.equals("codeflyr") && password.equals("pass123")) {
-			return "SUCCESS";
-		}
-		else {
-			return "ERROR";
-		}
+	
+	public String execute(){  
+	    if(this.validate(uname, password)){  
+	        return "SUCCESS";  
+	    }  
+	    else{  
+	        return "ERROR";  
+	    }  
+	}  
+
+	public boolean validate(String uname, String password) {
+		boolean status= false;
+		
+	      Connection conn = null;
+
+	      try {
+	    	 Class.forName("com.mysql.jdbc.Driver");
+	  	  	 conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/struts2database?serverTimezone=UTC", "root", "root");
+	  	  	
+	         String sql = "SELECT * FROM logincredentials WHERE userID = ? AND passwordID = ?";
+	         PreparedStatement ps = conn.prepareStatement(sql);
+	         ps.setString(1, uname);
+	         ps.setString(2, password);
+	         ResultSet rs = ps.executeQuery();
+	         status=rs.next();  
+	      }catch(Exception e){e.printStackTrace();}  
+
+		return status;
 	}
+	
 }
